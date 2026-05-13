b139a0af650d491fef8f354e3cc40721284f72cd
jchaeyun <jocheyoon@gmail.com>
2026-05-13 09:21:58 +0900

    Update model name to 'gemini-1.5-flash-latest'

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ with streamlit_analytics.track():
             with st.spinner("정보를 분석하고 있습니다..."):
                 try:
                     model = genai.GenerativeModel(
-                        model_name='gemini-1.5-flash',
+                        model_name='gemini-1.5-flash-latest',
                         tools=[{'google_search_retrieval': {}}] 
                     )
                     
